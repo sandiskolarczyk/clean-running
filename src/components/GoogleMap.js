189735f89fc834763f2d765e6388725bebe94875
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-07-02 15:43:44 +0100

    Add center, zoom to GoogleMap component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,18 +1,14 @@
 import { useRef, useState, useEffect } from 'react';
 
-export default function GoogleMap() {
-
-  const zoom = 14
-  const center = { lat: -0.131, lng: 51.501 }
-
+export default function GoogleMap({ center, zoom }) {
   const ref = useRef(null);
   const [map, setMap] = useState();
 
   useEffect(() => {
     if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {}));
+      setMap(new window.google.maps.Map(ref.current, { center, zoom }));
     }
-  }, [ref, map]);
+  }, [ref, map, center, zoom]);
 
-  return <div ref={ref} />
+  return <div ref={ref} id="map" />;
 }
--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,20 +1,27 @@
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import GoogleMap from "./GoogleMap";
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import GoogleMap from './GoogleMap';
 
-const API_KEY = "AIzaSyDgz-Iu4suXDMHGDFYxN1OBFYhtDWxEUPQ"
+const API_KEY = 'AIzaSyDgz-Iu4suXDMHGDFYxN1OBFYhtDWxEUPQ';
 
 export default function MapDisplay() {
-  console.log("inside MapDisplay")
+  console.log('inside MapDisplay');
+  const zoom = 14;
+  const center = { lat: -0.131, lng: 51.501 };
 
-  const render = (status) => {
-    console.log("inside render")
-    if (status === Status.FAILURE) console.log("failure")
+  const render = status => {
+    console.log('inside render');
+    if (status === Status.FAILURE) console.log('failure');
   };
 
-  return <Wrapper apiKey={API_KEY} render={render}>
-    <div style={{backgroundColor: "black"}}>
-      <GoogleMap style={{height: "100vh", width: "100vw"}}/>
-    </div>
-  </Wrapper>
-
+  return (
+    <Wrapper apiKey={API_KEY} render={render}>
+      <div style={{ backgroundColor: 'black' }}>
+        <GoogleMap
+          center={center}
+          zoom={zoom}
+          // style={{ height: '100vh', width: '100vw' }}
+        />
+      </div>
+    </Wrapper>
+  );
 }
