da71095bcdaf1d1bd5416530f9479546628447ae
Antony <countalong@gmail.com>
2022-07-02 15:18:20 +0100

    added linting setup

---
+++ src/utils/resultsScale.js
@@ -0,0 +1,27 @@
+scale = [
+    {
+        category: 'good',
+        index: 0-50,
+        description: 'Air quality is satisfactory, and air pollution poses little or no risk.',
+    },
+    {
+        category: 'moderate',
+        index: 51-100,
+        description: 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.',
+    },
+    {
+        category: 'good',
+        index: 101-150,
+        description: 'Members of sensitive groups may experience health effects. The general public is less likely to be affected.',
+    },
+    {
+        category: 'good',
+        index: 151-200,
+        description: 'Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.',
+    },
+    {
+        category: 'good',
+        index: 201,
+        description: 'Health alert: The risk of health effects is increased for everyone.',
+    }
+]
