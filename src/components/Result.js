d0e3c36ccf9cb1359633fd1ed17dd86254523e5f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-07-03 02:11:58 +0100

    feat: add line animation

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-import { resultInfo } from '../utils/resultInfo'
+import styled from 'styled-components';
+import { resultInfo } from '../utils/resultInfo';
 import { COLORS } from '../styles/colors';
 
 const LEVELS = {
-    good: COLORS.good,
-    moderate: COLORS.moderate,
-    sensitive: COLORS.sensitive,
-    unhealthy: COLORS.unhealthy,
-    "very unhealthy": COLORS.veryUnhealthy,
-    hazardous: COLORS.hazardous
-}
+  good: COLORS.good,
+  moderate: COLORS.moderate,
+  sensitive: COLORS.sensitive,
+  unhealthy: COLORS.unhealthy,
+  'very unhealthy': COLORS.veryUnhealthy,
+  hazardous: COLORS.hazardous,
+};
 
 const ResultWrapper = styled.div`
   display: flex;
   width: 500px;
+  /* height: 100px; */
   justify-content: space-between;
   align-items: center;
-  /* border: 1px solid black */
-`
+  padding: 5px 0;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: white;
+  box-shadow: 7px 8px 2px 1px rgba(0, 0, 255, 0.2);
+`;
+
+const CircleOut = styled.div`
+  background-color: white;
+  border-radius: 100%;
+  width: 50px;
+  height: 50%;
+`;
 
 const Circle = styled.div`
-  background-color: transparent;
+  background-color: ${props => props.color}78;
   border: 2px solid ${props => props.color};
   border-radius: 100%;
   width: 50px;
@@ -31,40 +43,34 @@ const Circle = styled.div`
   justify-content: center;
   align-items: center;
   margin-right: 10px;
-`
+`;
+
 const Level = styled.div`
   text-align: center;
   font-weight: 900;
   margin-right: 20px;
-`
+`;
 
 const Detail = styled.div`
-  width: 250px;
+  width: 300px;
   font-family: Sans-serif;
-`
-
-const drawLine = keyframes`
-
-`
-// @keyframes increase {
-//   100% {
-//       width: 300px;
-//   }
-// }
-
+`;
 
 export const Result = ({ aqi, category }) => {
-    return (
-      <ResultWrapper>
+  return (
+    <ResultWrapper>
+      <CircleOut>
         <Circle color={LEVELS[category.toLowerCase()]}>
           <p>{aqi}</p>
         </Circle>
-        <Level>
-          <p>{category.toUpperCase()}</p>
-        </Level>
-        <Detail>
-          <p>{resultInfo[category.toLowerCase()]}</p>
-        </Detail>
-      </ResultWrapper>
-    );
-  };
+      </CircleOut>
+
+      <Level>
+        <p>{category.toUpperCase()}</p>
+      </Level>
+      <Detail>
+        <p>{resultInfo[category.toLowerCase()]}</p>
+      </Detail>
+    </ResultWrapper>
+  );
+};
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,44 +1,88 @@
-import React from 'react'
+import React from 'react';
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-import { Result } from './Result'
-import { Line } from './Line';
+import { Result } from './Result';
 
 // mock data for now
 const results = [
-    {
-        aqi: 50,
-        category: "Sensitive"
-    },
-    {
-        aqi: 87,
-        category: "Very Unhealthy"
-    },
-    {
-        aqi: 17,
-        category: "Good"
-    },
-    {
-        aqi: 17,
-        category: "Good"
-    }
-]
+  {
+    aqi: 50,
+    category: 'Sensitive',
+  },
+  {
+    aqi: 87,
+    category: 'Very Unhealthy',
+  },
+  {
+    aqi: 17,
+    category: 'Good',
+  },
+  {
+    aqi: 37,
+    category: 'Moderate',
+  },
+  {
+    aqi: 300,
+    category: 'Hazardous',
+  },
+  {
+    aqi: 60,
+    category: 'Unhealthy',
+  },
+];
 
 const ResultsWrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  position: relative;
+  /* justify-content: center;
+  align-items: center; */
+  /* min-height: 100vh; */
+  max-height: 100%;
+  width: 500px;
+`;
+
+const lineAnimation = keyframes`
+0% {height: 0%;}
+100% {height: 85%;}
+`;
+
+const Animation = styled.div`
+  & {
+    background-color: #e0e0e0;
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    transform: translateX(-50%);
+    height: 85%;
+    width: 4px;
+    z-index: -1;
+    transition: 0.4s ease;
+    animation-name: ${lineAnimation};
+    animation-duration: 5s;
+  }
+
+  /* &::before {
+    content: '';
+    background-color: #e0e0e0;
+    position: absolute;
+    top: 0%;
+    left: 5%;
+    height: 200%;
+    width: 4px;
+    z-index: -2;
+  } */
+`;
 
 export const Results = () => {
   return (
     <ResultsWrapper>
-    {results.map((item, idx) => <Result key={idx} {...item}/>)}
-    <Line/>
+      <Animation />
+      {results.map((item, idx) => (
+        <Result key={idx} {...item} />
+      ))}
     </ResultsWrapper>
-  )
-}
+  );
+};
